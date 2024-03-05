0b137ac0b99015fd346e291a2571abda6da679a0
Zens <thaobaodh@gmail.com>
2024-03-05 13:48:46 +0700

    done

--- myproject/settings.py
+++ myproject/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'myproject1',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
     }
 }
 
